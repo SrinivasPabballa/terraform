ae39d66732c6718f235970e8a2c4eb02ac5a591e
SrinivasPabballa <srinivassunny64@gmail.com>
2025-11-19 15:50:07 +0530

    terraform

---
+++ variables/variables.tf
@@ -0,0 +1,40 @@
+variable "image_id"{
+    type = string
+    default = "ami-09c813fb71547fc4f"
+    description = "RHEL-9 image id" 
+}
+
+variable "instance_type" {
+     type = string
+     default = "t3.micro"
+}
+
+variable "tags"{
+    default = {
+        project = "Expense"
+        Environment = "Dev"
+        Module = "DB"
+        Name = "DB"
+    }
+}
+
+variable "sg_name"{
+    default = "allow_ssh"
+}
+
+variable "sg_description" {
+    default = "allowing SSH access"
+}
+
+variable "ssh_port"{
+    default = 22
+}
+
+variable "protocol"{
+    default = "tcp"
+}
+
+variable "allowed_cidr"{
+    type = list(string)
+    default = ["0.0.0.0/0"]
+}
